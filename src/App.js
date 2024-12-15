c8f2bf75fc7eb5cdb47f933d75a600e317e48758
iijawhor <jawhorali1@gmail.com>
2024-12-16 00:35:24 +0530

    feat: connect backend with frontend and implement user registration
    - Integrated backend API with frontend for user registration.
    - Successfully registered user data, including validation and error handling.
    - Uploaded avatar  to Cloudinary during registration.
    - Ensured appropriate responses and error messages from the API.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import { Register } from "./exportsFiles";
 function App() {
+  const [user, setUser] = useState(false);
+
+  if (!user) {
+    return (
+      <div className="appAuthContainer">
+        <Register />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <h1>Saji for Teacher</h1>
